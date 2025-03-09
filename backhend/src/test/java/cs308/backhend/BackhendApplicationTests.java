6f64b396ce4abd3ab6b233bfe22e14a0ce99c986
Mertcan Bakır <mertcan-bak@hotmail.com>
2025-03-09 14:43:36 +0300

    backhend

---
+++ backhend/src/test/java/cs308/backhend/BackhendApplicationTests.java
@@ -0,0 +1,13 @@
+package cs308.backhend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackhendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
