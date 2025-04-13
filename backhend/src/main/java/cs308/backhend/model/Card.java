afa91cb5547583959619895ba613528954088c53
zeynepgkmn <zeynep.gokmen@sabanciuniv.edu>
2025-04-13 15:14:21 +0300

    credit card number is encrypted

--- backhend/src/main/java/cs308/backhend/model/Card.java
+++ backhend/src/main/java/cs308/backhend/model/Card.java
@@ -1,6 +1,7 @@
 package cs308.backhend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import cs308.backhend.security.AESUtil;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -20,6 +21,22 @@ public class Card {
 
     @Column(nullable = true)
     private String CardNumber;
+
+    public void setCardNumber(String cardNumber) {
+        try {
+            this.CardNumber = AESUtil.encrypt(cardNumber);
+        } catch (Exception e) {
+            throw new RuntimeException("Encryption error", e);
+        }
+    }
+
+    public String getCardNumber() {
+        try {
+            return AESUtil.decrypt(this.CardNumber);
+        } catch (Exception e) {
+            throw new RuntimeException("Decryption error", e);
+        }
+    }
 
     @Column(nullable = true)
     private String cvv;
---
+++ backhend/src/main/java/cs308/backhend/security/AESUtil.java
@@ -0,0 +1,25 @@
+package cs308.backhend.security;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESUtil {
+    private static final String SECRET_KEY = "MySuperSecretKey"; // 16 byte olmalı (örnek)
+
+    public static String encrypt(String strToEncrypt) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encrypted = cipher.doFinal(strToEncrypt.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public static String decrypt(String strToDecrypt) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(strToDecrypt));
+        return new String(decrypted);
+    }
+}
